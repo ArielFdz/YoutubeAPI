e40d633ce6cfe1d92a81f44fb84d4a1d380b59d5
ArielFdz <arieljr11@outlook.com>
2024-01-11 09:33:12 -0600

    DiscordBot version1

--- DiscordBot/Program.cs
+++ DiscordBot/Program.cs
@@ -6,42 +6,93 @@ using Microsoft.Win32.SafeHandles;
 
 class Program
 {
-    private DiscordSocketClient _client;
-    public string sToken = "MTE5NDcwNTA3NjQzMDg0Mzk3NA.GDzUy-.OPmmWaOayr466f61_40-qSvNggulWRpnOhcVkg";
+    private readonly DiscordSocketClient _client;
 
-    static async Task Main(string[] args)
+    static void Main(string[] args)
+        => new Program()
+            .MainAsync()
+            .GetAwaiter()
+            .GetResult();
+
+    public Program()
     {
-        await new Program().MainAsync();
+        var config = new DiscordSocketConfig
+        {
+            GatewayIntents = GatewayIntents.AllUnprivileged | GatewayIntents.MessageContent
+        };
+
+        _client = new DiscordSocketClient(config);
+
+        _client.Log += LogAsync;
+        _client.Ready += ReadyAsync;
+        _client.MessageReceived += MessageReceivedAsync;
+        _client.InteractionCreated += InteractionCreatedAsync;
     }
 
     public async Task MainAsync()
     {
-        _client = new DiscordSocketClient();
+        await _client.LoginAsync(TokenType.Bot, "MTE5NDcwNTA3NjQzMDg0Mzk3NA.GMZ5Jg.eVsMWL2XwdTyzT6XepuPNw-3QeLfUOuIEs7JJ0");
 
-        _client.Log += Log;
-
-        await _client.LoginAsync(TokenType.Bot, sToken);
         await _client.StartAsync();
 
-        _client.MessageReceived += MessageReceived;
-
-        await Task.Delay(-1);
+        await Task.Delay(Timeout.Infinite);
     }
 
-    private Task Log(LogMessage arg)
+    private Task LogAsync(LogMessage log)
     {
-        Console.WriteLine(arg);
+        Console.WriteLine(log.ToString());
         return Task.CompletedTask;
     }
 
-    private async Task MessageReceived(SocketMessage arg)
+    private Task ReadyAsync()
     {
-        if (arg.Author.Id == _client.CurrentUser.Id || arg.Author.IsBot)
+        Console.WriteLine($"{_client.CurrentUser} is connected!");
+
+        return Task.CompletedTask;
+    }
+
+    private async Task MessageReceivedAsync(SocketMessage message)
+    {
+        if (message.Author.Id == _client.CurrentUser.Id)
             return;
 
-        if (arg.Content.Contains("!hola", StringComparison.OrdinalIgnoreCase))
+
+        if (message.Content.StartsWith("!metadata"))
         {
-            await arg.Channel.SendMessageAsync($"¡Hola, {arg.Author.Mention}!");
+            string urlVideo = message.Content.Substring("!metadata".Length).Trim();
+
+            using (HttpClient httpClient = new HttpClient())
+            {
+                HttpResponseMessage response = await httpClient.GetAsync($"https://localhost:7269/api/downloader/video?_urlVideo={urlVideo}");
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string responseBody = await response.Content.ReadAsStringAsync();
+                    await message.Channel.SendMessageAsync(responseBody);
+                }
+                else
+                {
+                    await message.Channel.SendMessageAsync($"Error al obtener metadata del video: {response.ReasonPhrase}");
+                }
+            }
+        }
+        else if (message.Content == "!ping")
+        {
+            var cb = new ComponentBuilder()
+                .WithButton("Click me!", "unique-id", ButtonStyle.Primary);
+            await message.Channel.SendMessageAsync("pong!", components: cb.Build());
+        }
+    }
+
+    private async Task InteractionCreatedAsync(SocketInteraction interaction)
+    {
+        if (interaction is SocketMessageComponent component)
+        {
+            if (component.Data.CustomId == "unique-id")
+                await interaction.RespondAsync("Thank you for clicking my button!");
+
+            else
+                Console.WriteLine("An ID has been received that has no handler!");
         }
     }
 }
